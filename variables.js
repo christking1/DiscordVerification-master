8a9f3cb521cbae0c8b436af3175103ac25f73366
christking1 <55986714+christking1@users.noreply.github.com>
2021-05-09 02:40:52 +0200

    Initial commit

---
+++ variables.js
@@ -0,0 +1,18 @@
+// Discord info
+const discord_bot_token = 'ODI0Nzk4Mjk1MTk1OTEwMTU0.YF0nRQ.xI2iK1dOgpEDemkE5eKshVaMe2o';
+const discord_guild_id = '824349585438146604';
+const discord_verified_role = 'adsadasdsad';
+
+// reCAPTCHA v2 secret key
+const recaptcha_secret_key = '6LdgX8waAAAAADiKs_QqYeZ2RnVYr8mCOypjAvQM';
+
+// Domain
+const domain = ''; // Leave empty for localhost
+
+module.exports = {
+    discord_bot_token,
+    discord_guild_id,
+    discord_verified_role,
+    recaptcha_secret_key,
+    domain
+}
